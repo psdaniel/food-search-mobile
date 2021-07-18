d2863097b16c340945eafdcec24266eb1653adde
Daniel <daniel.n10@hotmail.com>
2021-07-18 19:30:56 -0300

    Add resultsshow to stack navigator

---
+++ src/components/ResultsList.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import ResultsDetail from './ResultsDetail';
+
+const ResultsList = ({ title, results, navigation }) => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>{title}</Text>
+            <FlatList
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                data={results}
+                keyExtractor={(result) => result.id}
+                renderItem={({ item }) => {
+                    return (
+                        <TouchableOpacity onPress={() => navigation.navigate('ResultsShow')}>
+                            <ResultsDetail result={item} />
+                        </TouchableOpacity>
+                    )
+                }}
+            />
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginLeft: 15,
+        marginBottom: 5
+    },
+    container: {
+        marginBottom: 10,
+    }
+});
+
+export default ResultsList;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,21 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
-const SearchScreen = () => {
+const SearchScreen = ({ navigation }) => {
     const [term, setTerm] = useState('');
     const [searchApi, results, errorMessage] = useResults();
 
+    const filterResultsByPrice = (price) => results
+        .filter(result => result.price === price);
+
     return (
-        <View>
+        <View style={{ flex: 1 }}>
             <SearchBar
                 term={term}
                 onTermChange={setTerm}
                 onTermSubmit={() => searchApi(term)}
             />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
-            <Text>We have found {results.length} results</Text>
+            <ScrollView>
+                <ResultsList
+                    results={filterResultsByPrice('$')}
+                    title="Cost Effective"
+                    navigation={navigation}
+                />
+                <ResultsList
+                    results={filterResultsByPrice('$$')}
+                    title="Bit Pricier"
+                    navigation={navigation}
+                />
+                <ResultsList
+                    results={filterResultsByPrice('$$$')}
+                    title="Big Spender"
+                    navigation={navigation}
+                />
+            </ScrollView>
         </View>
     )
 };
