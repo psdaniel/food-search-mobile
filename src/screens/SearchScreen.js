864aeed44cac7d26c6f82d27e426be7d76f079c9
Daniel <daniel.n10@hotmail.com>
2021-07-18 12:17:16 -0300

    Create state variable to handle filter text

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from './components/SearchBar';
 
 const SearchScreen = () => {
+    const [term, setTerm] = useState('');
+
     return (
         <View>
-            <SearchBar />
+            <SearchBar
+                term={term}
+                onTermChange={newTerm => setTerm(newTerm)}
+                onTermSubmit={() => console.log('term was submitted')}
+            />
             <Text>Search Screen</Text>
-        </View>)
+            <Text>{term}</Text>
+        </View>
+    )
 };
 
 const styles = StyleSheet.create({});
--- src/screens/components/SearchBar.js
+++ src/screens/components/SearchBar.js
@@ -1,19 +1,40 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, TextInput, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons'
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
     return (
-        <View style={styles.background}>
-            <Text>Search Bar</Text>
+        <View style={styles.backgroundStyle}>
+            <Feather name="search" style={styles.iconStyle} />
+            <TextInput
+                style={styles.inputStyle}
+                placeholder="Search"
+                value={term}
+                autoCapitalize="none"
+                autoCorrect={false}
+                onChangeText={onTermChange}
+                onEndEditing={onTermSubmit}
+            />
         </View>
     )
 };
 
 const styles = StyleSheet.create({
-    background: {
-        backgroundColor: '#F0EEE',
+    backgroundStyle: {
+        marginTop: 10,
+        backgroundColor: '#F0EEEE',
         height: 50,
         borderRadius: 5,
+        marginHorizontal: 15,
+        flexDirection: 'row'
+    },
+    inputStyle: {
+        flex: 1,
+        fontSize: 18
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center',
         marginHorizontal: 15
     }
 });
